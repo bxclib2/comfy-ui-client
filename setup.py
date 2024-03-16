ccfe966ab12eb8a019543308abad494f008a7eab
bxc <xingce.bao@gmail.com>
2024-03-17 03:42:28 +0800

    v0.0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='comfy-ui-client',
-    version='0.0.1.4',
+    version='0.0.1.5',
     license='MIT',
     author='Gavin Bao',
     author_email='xingce.bao@gmail.com',
